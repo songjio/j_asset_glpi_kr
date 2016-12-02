c0240a0694c5cbf8ceddc5f2aae74223ef11e09b
songjio <songjo@ji-in.com>
2016-12-02 15:18:48 +0900

    Update consumableitem.class.php

--- inc/consumableitem.class.php
+++ inc/consumableitem.class.php
@@ -208,7 +208,8 @@ class ConsumableItem extends CommonDBTM {
       
       ///////////////////////////////////////////////////////////////////////////////////////////////
       if($ID > 0){
-      	$docdb=mysql_connect("localhost","root","jiin0701");
+                                             //db password
+      	$docdb=mysql_connect("localhost","root","");
       	mysql_select_db("glpi_db",$docdb);
       
       	$docquery="select a.mime, b.documents_id, b.itemtype
@@ -223,7 +224,6 @@ class ConsumableItem extends CommonDBTM {
       		$org = $row[mime];
       		$indexlen  = strpos($org, "/");
       		$resultstr = substr($org,0,$indexlen);
-      		echo $resultstr;
       		if($resultstr=="image"){
       			echo "<tr class='tab_bg_1'>";
       			echo "<td>".__('image')."</td>";
